f02731e4d83a6b6264e91286b6d6807c1a6400b4
Reynald BERRIOT <rberriot@vente-privee.com>
2020-01-18 19:44:50 +0100

    step3 add arguments parser

--- Biz.Console/Program.cs
+++ Biz.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Biz.Common;
+using Fclp;
 using Newtonsoft.Json;
 using System;
 using System.Linq;
@@ -8,28 +9,64 @@ namespace Biz.Console
 {
     class Program
     {
+        public enum OutputType
+        {
+            PlainText = 1,
+            Json = 2,
+            Xml = 3
+        }
+        public class ApplicationArguments
+        {
+            public string File { get; set; }
+            public OutputType OutputType { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            if (args.Length != 1)
+            // create a generic parser for the ApplicationArguments type
+            var p = new FluentCommandLineParser<ApplicationArguments>();
+
+            p.Setup(arg => arg.File)
+             .As('f', "file") 
+             .Required();
+
+            p.Setup(arg => arg.OutputType)
+             .As('o', "output_type")
+             .SetDefault(OutputType.PlainText);
+
+            var result = p.Parse(args);
+
+            if (result.HasErrors)
             {
-                System.Console.WriteLine("Please a file path is necessary !");
                 Environment.Exit(-1);
             }
+            Run(p.Object);
+        }
+
+        public static IFormatterEnumerable<OutputRowModel> GetFormatter(OutputType type)
+        {
+            switch (type)
+            {
+                case OutputType.Json:
+                    return new JsonFormatterEnumerableOutputRowModel();
+                case OutputType.Xml:
+                    return new XmlFormatterEnumerableOutputRowModel();
+                //case OutputType.PlainText:
+                //    return new PlainTextFormatterEnumerableOutputRowModel();
+            }
+            return new PlainTextFormatterEnumerableOutputRowModel();
+        }
+
+        public static void Run(ApplicationArguments arg) {
 
             var mapper = new MapperConfiguration(cfg => _ = MapperHelper.Configure(cfg)).CreateMapper();
 
-            var rows = new PlainTextFormatterEnumerableOutputRowModel().Serialize(CsvHelperProxy
-                .ReadRows(args[0])
+            var rows = GetFormatter(arg.OutputType).Serialize(CsvHelperProxy
+                .ReadRows(arg.File)
                 .Skip(1)
                 .Select((v, i) => mapper.Map<RowModel>((i, v)))
                 .Select(mapper.Map<OutputRowModel>)
                 );
-            //var rows = new JsonFormatterEnumerableOutputRowModel().Serialize(CsvHelperProxy
-            //    .ReadRows(args[0])
-            //    .Skip(1)
-            //    .Select((v, i) => mapper.Map<RowModel>((i, v)))
-            //    .Select(mapper.Map<OutputRowModel>)
-            //    );
 
             foreach (var row in rows)
             {
