8c89835ceaa452d01e556a6e2c8271db32d443b8
Reynald BERRIOT <rberriot@vente-privee.com>
2020-01-19 12:57:56 +0100

    part 2 - step 1 -> empty api with swagger

---
+++ Biz.Api/Controllers/CsvController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biz.Api.Controllers
+{
+    [Route("api/[controller]/filter")]
+    [ApiController]
+    public class CsvController : ControllerBase
+    {
+        [HttpGet]
+        public async Task GetCsvAsync([FromQuery, Required] string csvUri)
+            => await Task.CompletedTask;
+
+        [HttpPost]
+        public async Task PostCsvAsync([FromBody] string csvUri)
+            => await Task.CompletedTask;
+    }
+}
---
+++ Biz.Api/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace Biz.Api
+{
+    public class Startup
+    {
+        private static readonly string SwaggerPrefix = "api/swagger";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddMvc()
+                    .AddNewtonsoftJson()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "my little api", Version = "v1" });
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "Biz.Api.xml");
+                if (File.Exists(xmlPath))
+                    c.IncludeXmlComments(xmlPath);
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseSwagger(o => o.RouteTemplate = SwaggerPrefix + "/{documentName}/swagger.json");
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/{SwaggerPrefix}/v1/swagger.json", "My API V1");
+                c.RoutePrefix = SwaggerPrefix;
+            });
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+
+    }
+}
